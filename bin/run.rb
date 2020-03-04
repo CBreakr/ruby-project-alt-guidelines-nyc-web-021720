d4fe3c33627214475c57e708a1bc39d37a5154ed
CBreakr <github email address>
2020-03-04 13:46:58 -0500

    made overall CLI startup shell

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,15 @@
 require_relative '../config/environment'
 
+# welcome!
+puts "Welcome to your Adventure!"
+
+# get the user
+puts "Who are you?"
+user_name = gets.chomp
+user = User.find_or_create_by(name: user_name)
+
+# run the app
+Context.new.start(MainMenuState.new, user)
+
+# after user quits
+puts "Come back any time!"
