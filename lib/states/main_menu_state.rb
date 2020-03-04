99dbca7a4c129627d7dd8453ef643ed9585e83b0
CBreakr <github email address>
2020-03-04 14:36:30 -0500

    refactored out back, quit, interpret functonality into parent state class

--- lib/states/main_menu_state.rb
+++ lib/states/main_menu_state.rb
@@ -1,22 +1,25 @@
 require_relative "./state"
 
 class MainMenuState < State
+
+    def initialize
+        @use_back = false
+    end
     
     def start_up(input_value, user)
         ["Create new campaign", "View existing campaigns"]
     end
 
-    def interpret(options, input_value, user, choice)
-        choice_num = choice.to_i
-        
-        if  choice.to_i > 0 ||  choice_num <= options.length
-            if choice == 1
-                # [CreateCampaignState.new, nil]
-            else
-                # [SelectCampignState.new,  nil]
-            end
+    def select_option(options, input_value, user, choice_num)     
+        puts "chose option: #{choice_num}"
+        if choice_num == 1 then
+            puts "you chose: #{options[choice_num-1]}"
+            puts "should be Create new campiagn"
+            # [CreateCampaignState.new, nil]
         else
-            false
-        end 
+            puts "you chose: #{options[choice_num-1]}"
+            puts "should be View existing campaign"
+            # [SelectCampignState.new,  nil]
+        end
     end
 end
--- lib/states/state.rb
+++ lib/states/state.rb
@@ -1,10 +1,18 @@
 class State
+    attr_accessor :use_back
+
+    def initialize
+        @use_back = true
+    end
+
+    #
     def run(input_value, user)
         options = start_up(input_value, user)
         display_options(options)
         evaluate(options, input_value, user)
     end
     
+    #
     def start_up(input_value, user)
     end
 
@@ -13,25 +21,67 @@ class State
         options.each_with_index do |option, index|
             puts "#{index + 1}: #{option}"
         end
-    end
 
-    def interpret(options, input_value, user, choice)
+        if(@use_back) then
+            puts "enter b to go back to previous menu"
+        end
+        puts "enter q to quit"
     end
 
     # return is going to be an array [next_state, output_value]
     def evaluate(options, input_value, user)
         choice = gets.chomp
-        result = interpret(options, input_value, user, choice)
+        
+        result = interpret_defaults(options, input_value, user, choice)
+
+        result ||= interpret(options, input_value, user, choice)
+        
         # if result is nonsense, then re-run evaluate
         # if result is back, the go back
         # if result is quit, then return an array [nil, nil]
         # if result is finish, then perform finish
 
-        if result
+        if result then
             result
         else
             evaluate(options, input_value, user)
         end
+    end
 
+    #
+    def interpret_defaults(options, input_value, user, choice)
+        if (@use_back && choice == "b") then
+            run_back(options, input_value, user, choice)
+        elsif(choice == "q") then
+            return [nil, nil]
+        end
+
+        return nil
+    end
+
+    #
+    def run_back(options, input_value, user, choice)
+        puts "go back!"
+        false
+    end
+
+    #
+    def interpret(options, input_value, user, choice)        
+        choice_num = choice.to_i
+        
+        if valid_choice?(choice_num, options) then
+            select_option(options, input_value, user, choice_num)
+        else
+            false
+        end
+    end
+
+    #
+    def select_option(options, input_value, user, choice_num)
+    end
+
+    #
+    def valid_choice?(choice_num, options)
+        choice_num > 0 && choice_num <= options.length
     end
 end
