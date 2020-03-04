253285dbf1e681687af1a1d04794e365aae2a0d1
github username <github email address>
2020-03-04 16:56:53 -0500

    created CreateCampaignState

--- lib/states/create_campaign_state.rb
+++ lib/states/create_campaign_state.rb
@@ -1,10 +1,20 @@
 class CreateCampaignState < State
 
     def start_up(input_value, user)
+        puts "These are the available levels"
+        puts "Which level do you want to add to your Campaign?"
         Dungeon.all.map {|dungeon| dungeon.name}
     end
 
-    def select_option(options, input_value, user, choice_num)
+    # def run_back(options, input_value, user, choice)
+        
+    # end
 
+    def select_option(options, input_value, user, choice_num)
+        dungeon_name = options[choice_num -1]
+        puts "you added level #{dungeon_name}"
+        found_dun = Dungeon.find_by(name: dungeon_name)
+        Campaign.create(user: user, dungeon: found_dun)
+        [MainMenuState.new, nil]
     end
 end
