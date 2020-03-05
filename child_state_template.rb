f260cb09f4c5691d64908c1b5ceb37131586494a
CBreakr <github email address>
2020-03-05 13:23:50 -0500

    created child state template

---
+++ child_state_template.rb
@@ -0,0 +1,15 @@
+require_relative './'
+require_relative './'
+
+class CampaignDetailState < State
+    def start_up(input_value, user_id)
+        []
+    end
+
+    def run_back(options, input_value, user_id, choice)
+        []
+    end
+
+    def select_option(choice, input_value, user_id)
+    end
+end
